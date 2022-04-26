e9bf988bb6791a9066f141d448d657fe7498f365
Hrishigawde <88854176+Hrishigawde@users.noreply.github.com>
2022-04-26 21:45:39 +0100

    Driver Changes

--- src/test/java/TestCases/AirbusJobSearch.java
+++ src/test/java/TestCases/AirbusJobSearch.java
@@ -1,19 +1,14 @@
 package TestCases;
 
-import io.cucumber.java.After;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.junit.Cucumber;
-import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pageObjects.AirbusCareerHomePage;
 import pageObjects.PopUp;
@@ -27,36 +22,36 @@ import java.util.concurrent.TimeUnit;
 
 
 @Test
+@RunWith(Cucumber.class)
 
 public class AirbusJobSearch extends DriverBase {
 
-    WebDriver stepDefDriver;
 
-    public void driverNavigation() throws IOException {
-        stepDefDriver = initializeDriver();
-        stepDefDriver.get("https://www.airbus.com/en/careers");
+   public WebDriver driver = initializeDriver();
+
+    public AirbusJobSearch() throws IOException {
+        driver.get("https://www.airbus.com/en/careers");
     }
-
 
     @Given("User is on landing page follow the link to the Workday Jobs List")
     public void userIsOnLandingPageFollowTheLinkToTheWorkdayJobsList() {
-       AirbusCareerHomePage rd1 = new AirbusCareerHomePage(stepDefDriver);
+       AirbusCareerHomePage rd1 = new AirbusCareerHomePage(driver);
        rd1.OpenWorkdayPage().click();
     }
 
     @When("Using the filters user should be able to locate the role and apply")
     public void usingTheFiltersUserShouldBeAbleToLocateTheRole() throws InterruptedException {
 
-       SearchPosition rd2 = new SearchPosition(stepDefDriver);
-        Set<String> windows = stepDefDriver.getWindowHandles();  // parent id -> child id
+       SearchPosition rd2 = new SearchPosition(driver);
+        Set<String> windows = driver.getWindowHandles();  // parent id -> child id
         Iterator<String> it = windows.iterator();
         String parentId = it.next();
         String childId = it.next();
-        stepDefDriver.switchTo().window(childId);
+        driver.switchTo().window(childId);
 
         rd2.LocationButton().click();
         rd2.SearchBoxText().sendKeys("Uni");
-        stepDefDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         if (!rd2.getCountryDynamicOption().isEmpty())
             for (WebElement option : rd2.getCountryDynamicOption()) {
@@ -84,11 +79,9 @@ public class AirbusJobSearch extends DriverBase {
         Thread.sleep(5000);
 
         for (WebElement jobNameOptions : rd2.allJobsSearch()) {
-            System.out.println("Job names are "+ jobNameOptions.getText());
             if("Software / Hardware Test and Integration Engineer".equalsIgnoreCase(jobNameOptions.getText())){
-
-                jobNameOptions.click();
-                break;
+                    jobNameOptions.click();
+                    break;
             }
 
         }
@@ -96,14 +89,12 @@ public class AirbusJobSearch extends DriverBase {
         rd2.clickApply().click();
     }
 
-    @AfterTest
     @Then("detect that the pop up is present on the screen")
     public void checkPopupDisplayed() throws InterruptedException {
-
-        PopUp rd3 =new PopUp(stepDefDriver);
+        //checking pop-up
+        PopUp rd3 =new PopUp(driver);
         Assert.assertEquals(rd3.popUpDisplay().getText(),"Start Your Application");
         System.out.println(rd3.popUpDisplay().getText());
-
 
     }
 }
